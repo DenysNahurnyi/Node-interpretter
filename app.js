2ccd7b92cfa40a3076bb91732f8dace0432e34ea
Denys Nahurnyi <denysnahyrnui@gmail.com>
2017-10-02 21:12:09 +0300

    without comment

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ GlobalContext.code = []
 let text = (fs.readFileSync(`${__dirname}/files/text.txt`)).toString();
 let code = (fs.readFileSync(`${__dirname}/files/code.txt`)).toString();
 
-// console.log(`${code} and ${text}`);
-
 exports.isFunction = function (str) {
     return /\w+[(].+[)]/.test(str)
 }
--- functions/funcResult.js
+++ functions/funcResult.js
@@ -16,26 +16,19 @@ exports.funcResult = function(func, lineDescribeObject, GlobalContext) {
         }
         case 'IndexOf': {
             let result = /^IndexOf[(](.+), (.+)[)]/.exec(func);
-            console.log(`Results of exec : ${result[1]} and ${result[2]}`)
             let value = result[1], string = result[2];
             if(app.isFunction(value)) {
                 while(app.isFunction(value)){
-                    console.log(`Result 1 before transform: ${value}`)
                     value = exports.funcResult(value, lineDescribeObject, GlobalContext)
-                    console.log(`Result 1 after transform: ${value} and it's function: ${app.isFunction(value)}`)    
                 }            
             } else {
                 value = GlobalContext[result[1]];
-                console.log(`Value 1 : ${value}`)
             }
             if(app.isFunction(string)) {
                 while(app.isFunction(string)){
-                    console.log(`Result 1 before transform: ${string}`)
                     value = exports.funcResult(string, lineDescribeObject, GlobalContext)
-                    console.log(`Result 1 after transform: ${string} and it's function: ${app.isFunction(string)}`)    
                 }            
             } else {
-                console.log(`Value 2: ${GlobalContext[result[2]]}`)
                 string = GlobalContext[result[2]].toString();
             }
             lineDescribeObject.comment = "Processing fuction IndexOf";
